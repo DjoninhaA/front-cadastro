f023b91d720177a46362d1b739256827aaef9123
djoninhaA <djonathan8016@gmail.com>
2025-03-10 20:12:21 -0300

    Feat: estilizando rota de listagem

--- src/app/lista/page.tsx
+++ src/app/lista/page.tsx
@@ -1,28 +1,54 @@
 "use client";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+
+type User = {
+  id: string;
+  name: string;
+  email: string;
+};
 
 export default function ListarUsuarios() {
+  const [allUsers, setAllUsers] = useState<User[]>([]); // Inicializando como array vazio
+
   useEffect(() => {
     async function loadusers() {
       const token = localStorage.getItem("token");
-      console.log(token);
-      const response = await fetch("http://localhost:3000/listar", {
-        method: "GET",
-        headers: {
-          //   "Content-Type": "application/json",
-          Authorization: `Bearer ${token}`,
-        },
-      });
 
-      console.log(response);
+      try {
+        const response = await fetch("http://localhost:3000/listar", {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+
+        if (!response.ok) {
+          console.error("Erro ao carregar usuários:", response.statusText);
+          return;
+        }
+
+        const data = await response.json();
+        setAllUsers(data.users);
+      } catch (error) {
+        console.error("Erro ao fazer a requisição:", error);
+      }
     }
 
     loadusers();
   }, []);
 
   return (
-    <div>
-      <h2>Listar Usuarios</h2>
+    <div className="max-w2xl mx-auto mt-10 bg-white p-8">
+      <h2 className="text-2xl font-bold mb-6 text-center  text-gray-800">Lista de Usuários</h2>
+      <ul className="space-y-2 ">
+        {allUsers.map((user) => (
+          <li key={user.id} className="bg-gray-100 p-4 rounded-lg text-center max-w-md mx-auto mt-10">
+            <p className="font-semibold">ID: {user.id}</p>
+            <p className="font-semibold">Nome: {user.name}</p>
+            <p className="font-semibold">Email: {user.email}</p>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
