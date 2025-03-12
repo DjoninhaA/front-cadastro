9630af52b71d8ec861c46ff4bdab5518f44e88c4
djoninhaA <djonathan8016@gmail.com>
2025-03-12 12:51:57 -0300

    feat: redirecionamento

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,18 @@
 export default function CenteredText() {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-900 text-white">
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-900 text-white">
       <h1 className="text-6xl sm:text-8xl md:text-9xl font-bold text-center 
         bg-gradient-to-r from-purple-400 to-pink-500 bg-clip-text text-transparent
         drop-shadow-lg">
         Bem Vindo!
       </h1>
+      <a
+        href="/cadastro"
+        className="w-40 text-center bg-gradient-to-r from-purple-500 to-pink-500 text-white py-2 px-4 rounded-xl 
+          hover:from-purple-400 hover:to-pink-400 transition-all duration-300 drop-shadow-lg text-lg font-semibold"
+      >
+        Cadastrar-se
+      </a>
     </div>
   );
 }
